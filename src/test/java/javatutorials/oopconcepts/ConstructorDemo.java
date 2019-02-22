d40faa02808e9aeead83e76f4a4be16a2614ba3a
havuong <havuong138@gmail.com>
2019-02-22 11:45:24 +0700

    Add - Condition Statements and Loops

--- src/test/java/javatutorials/oopconcepts/ConstructorDemo.java
+++ src/test/java/javatutorials/oopconcepts/ConstructorDemo.java
@@ -2,20 +2,20 @@ package javatutorials.oopconcepts;
 
 public class ConstructorDemo {
 
-	public static void main(String[] args) {
-		
-		CarConstructor c1 = new CarConstructor();
-		
-		c1.setMake("BMW");
-		System.out.println(c1.getMake());
-		
-		System.out.println(c1.speed);
-		System.out.println(c1.gear);
-		
-		System.out.println("*********************");
+    public static void main(String[] args) {
 
-		CarConstructor c2 = new CarConstructor(10, 1);
-		System.out.println(c2.speed);
-		System.out.println(c2.gear);
-	}
+        CarConstructor c1 = new CarConstructor();
+
+        c1.setMake("BMW");
+        System.out.println(c1.getMake());
+
+        System.out.println(c1.speed);
+        System.out.println(c1.gear);
+
+        System.out.println("*********************");
+
+        CarConstructor c2 = new CarConstructor(10, 1);
+        System.out.println(c2.speed);
+        System.out.println(c2.gear);
+    }
 }
